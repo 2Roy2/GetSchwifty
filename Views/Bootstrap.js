ae024b0d1c47a9bdeb3a948991cc85d034770b21
2Roy2 <royarad5@gmail.com>
2022-07-03 19:10:03 +0300

    added updateBoard and used it in bootstrap

--- Views/Bootstrap.js
+++ Views/Bootstrap.js
@@ -1,6 +1,12 @@
 import { Board } from '../Common/Board.js';
 import {createBlocks} from './CreateBoard.js';
 import {Block} from '../Common/Block.js';
+import {updateBoard} from './UpadateBoard.js';
 
-let board = new Board([[new Block(1),new Block(56),new Block(3)],[new Block(1),new Block(2),new Block(3)],[new Block(1),new Block(2),new Block(3)]])
+let board = new Board([[new Block(1),new Block(2),new Block(3)],[new Block(1),new Block(2),new Block(3)],[new Block(1),new Block(2),new Block(3)]])
 createBlocks(board);
+
+let updatedBoard = new Board([[new Block(6),new Block(6),new Block(6)],[new Block(6),new Block(6),new Block(6)],[new Block(6),new Block(6),new Block(6)]])
+await new Promise(r => setTimeout(r, 2000));
+
+updateBoard(updatedBoard)
