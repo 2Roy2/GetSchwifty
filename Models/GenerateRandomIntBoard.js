39bdd5084614d087566f2a8bf7e9bc076419f03e
2Roy2 <royarad5@gmail.com>
2022-07-03 20:22:40 +0300

    added generateRandomBoard model

---
+++ Models/GenerateRandomIntBoard.js
@@ -0,0 +1,34 @@
+import { Board } from '../Common/Board.js';
+
+
+function removeValueFromValues(values,value){
+    return values.filter((element)=> element!==value) 
+}
+function getBoxValues(boardSize){
+    let values = new Array();
+    for(let i = 0; i< boardSize;i++){
+        values.push(i);
+    }
+    return values;
+}
+function generateRow(rowSize,values){
+    let row = new Array();
+    for(let i =0;i<rowSize;i++){
+        let value =  values[Math.floor(Math.random()*values.length)];
+        row.push(value);
+        values=removeValueFromValues(values,value); 
+    }
+    let newValues = values;
+    return {row,newValues};
+}
+
+ export function generateRandomBoard(columns,rows){
+    let board = new Array();
+    values = getBoxValues(columns*rows);
+    for(let i =0;i<rows;i++){
+        let {row,newValues}=generateRow(columns,values);
+        board.push(row);
+        values=newValues;
+    }
+    return new Board(board);
+}
