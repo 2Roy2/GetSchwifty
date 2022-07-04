2db6c1035042bc2b07bfa69425dd6c74743314b1
2Roy2 <royarad5@gmail.com>
2022-07-04 19:33:55 +0300

    added more parameters to Board

--- Common/Board.js
+++ Common/Board.js
@@ -1,5 +1,7 @@
 export class Board{
     constructor(blocks){
         this.blocks=blocks;
+        this.gameLength=0;
+        this.stratTime=new Date();
     }
 }
--- Controllers/GenerateBoard/NumberBlocks.js
+++ Controllers/GenerateBoard/NumberBlocks.js
@@ -2,7 +2,8 @@ import { Block } from "../../Common/Block.js";
 
 export function getBoxValues(boardSize){
     let values = new Array();
-    for(let i = 0; i< boardSize;i++){
+    values.push(new Block(0,'#'));
+    for(let i = 1; i< boardSize;i++){
         values.push(new Block(i,i));
     }
     return values;
