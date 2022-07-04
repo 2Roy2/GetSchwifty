41a86b93a0f7176c3327a844e17d6f291634840e
2Roy2 <royarad5@gmail.com>
2022-07-04 10:17:57 +0300

    changed bootstrap to be more intoative

--- Views/Bootstrap.js
+++ Views/Bootstrap.js
@@ -6,13 +6,15 @@ import {generateRandomBoard} from '../Controllers/GenerateBoard/GenerateRandomBo
 import { getBoxValues } from '../Controllers/GenerateBoard/IntValues.js';
 import{getOpposite} from '../Controllers/BordValidators/OppositeBoardGetter.js'
 
+let rows =4;
+let columns=4;
+let size =rows*columns;
 
-let board =generateRandomBoard(3,3,getBoxValues(9));
-
+let board =generateRandomBoard(rows,columns,getBoxValues(size));
 createBlocks(board);
-console.log(getOpposite(board,[0,0]));
+// console.log(getOpposite(board,[0,0]));
 
-let updatedBoard = generateRandomBoard(3,3,getBoxValues(9));
-await new Promise(r => setTimeout(r, 2000));
+// let updatedBoard = generateRandomBoard(3,3,getBoxValues(9));
+// await new Promise(r => setTimeout(r, 2000));
 
-updateBoard(updatedBoard)
+// updateBoard(updatedBoard)
--- Views/UpadateBoard.js
+++ Views/UpadateBoard.js
@@ -2,7 +2,6 @@
 export function updateBoard(board){
     var buttons = document.getElementsByTagName('button');
     let blocks = board.blocks.flat();
-    console.log(blocks);
     for (let i = 0; i <blocks.length; i++) {
         buttons[i].innerHTML=blocks[i].val;
     }
